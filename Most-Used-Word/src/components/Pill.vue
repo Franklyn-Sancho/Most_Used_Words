a5cde738cfbbc48ad844fcf1f386cf88009dceec
Franklyn-Sancho <franklyn.hs@gmail.com>
2020-09-05 20:05:21 -0300

    Initial commit

---
+++ Most-Used-Word/src/components/Pill.vue
@@ -0,0 +1,50 @@
+<template>
+    <div class="pill">
+        <span class="name">{{ name }}</span>
+        <span class="amount">{{ amount }}</span>
+
+    </div>
+  
+</template>
+
+<script>
+export default {
+    props: ['name', 'amount']
+
+}
+</script>
+
+<style>
+.pill {
+    margin: 10px;
+    height: 55px;
+    border-radius: 30px;
+    background-color: crimson;
+    color: #fff;
+    font-size: 1.3rem;
+    font-weight: bold;
+    display: flex; 
+    align-items: center;
+    }
+
+    .pill .name {
+        margin-left: 15px;
+    }
+
+    .pill .amount {
+        margin: 10px;
+        padding: 5px;
+        height: 35px;
+        widows: 35px;
+        border-radius: 18px;
+        background-color: #fff;
+        color: #dc143c;
+        font-size: 14px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+
+
+
+</style>
